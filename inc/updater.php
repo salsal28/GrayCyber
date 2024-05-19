6929fb0caf869ab392f16dce13a13b7e1e2706bb
Saja01 Zarour <54006733+salsal28@users.noreply.github.com>
2024-05-19 16:28:04 +0300

    Updates

--- inc/updater.php
+++ inc/updater.php
@@ -1,35 +1,5 @@
 <?php
 
-/*
-Plugin Name: WordPress.com Theme Updates
-Description: Update themes downloaded from WordPress.com seamlessly with the rest of your WordPress updates.
-Author: Automattic
-Version: 1.0
-Author URI: http://automattic.com/
-License: GPL v2 or later
-Network: true
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
-/**
- * Hooks to update themes from WordPress.com using the native WordPress upgrader
- * @package  WordPress.com Updater
- * @version  1.0
- */
-
 if ( ! class_exists( 'WPCom_Theme_Updater') ) :
 
 class WPCom_Theme_Updater {
